997ea7f42ca884755a724f0553ffe2f03f62972a
Vu Nguyen <vu@robomatter.com>
2015-05-22 15:09:18 -0400

    Bumping the max file size to 64M

--- php_settings_override/recipes/default.rb
+++ php_settings_override/recipes/default.rb
@@ -7,7 +7,7 @@ file "/etc/php5/apache2/conf.d/cs2n-learn.ini" do
   
   ; Maximum allowed size for uploaded files.
   ; http://php.net/upload-max-filesize
-  upload_max_filesize = 16M
+  upload_max_filesize = 64M
  
   EOH
   notifies :restart, resources(:service => 'apache2')
