44cdb981f5f8fce2063189a186b7a4ef37662c6d
Vu Nguyen <vnguyen@rec.ri.cmu.edu>
2015-07-09 16:37:15 -0400

    Added new cron to purge cache for moodle every 12 hours.

--- cron/recipes/default.rb
+++ cron/recipes/default.rb
@@ -2,3 +2,9 @@ cron "moodle_cron" do
   minute "*/59"
   command "php /srv/www/cs2n_learn/current/admin/cli/cron.php"
 end
+
+cron "moodle_purge_cache" do
+  hour "12"
+  minute "0"
+  command "php /srv/www/cs2n_learn/current/admin/cli/purge_caches.php"
+end
