c018f99f9485c740a38037a094d07d3e077ea85b
Vu Nguyen <vu@robomatter.com>
2015-08-17 15:20:15 -0400

    Trying to fix the upload size limit

--- php_settings_override/recipes/default.rb
+++ php_settings_override/recipes/default.rb
@@ -8,6 +8,7 @@ file "/etc/php5/apache2/conf.d/cs2n-learn.ini" do
   ; Maximum allowed size for uploaded files.
   ; http://php.net/upload-max-filesize
   upload_max_filesize = 64M
+  post_max_size = 64M
  
   EOH
   notifies :restart, resources(:service => 'apache2')
