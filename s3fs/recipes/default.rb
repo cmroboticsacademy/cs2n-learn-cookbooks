c1767f8f9b7c049c1052eedf5c666421e4b670db
Vu Nguyen <vnguyen@robomatter.com>
2016-01-05 11:11:29 -0500

    Updating the s3fs source url since it moved.

--- s3fs/recipes/default.rb
+++ s3fs/recipes/default.rb
@@ -3,7 +3,7 @@
 end
 
 remote_file "/tmp/fuse-2.9.3.tar.gz" do
-  source "http://downloads.sourceforge.net/project/fuse/fuse-2.X/2.9.3/fuse-2.9.3.tar.gz"
+  source "https://github.com/libfuse/libfuse/releases/download/fuse_2_9_4/fuse-2.9.3.tar.gz"
   mode 0644
 end
 
